d8ffd6ba8668456d04d3a79212de20f4e0fb9a48
mozart953 <wilsonnianopop@gmail.com>
2023-03-05 20:29:31 -0600

    some changes 2

--- enMachineP/magiq.py
+++ enMachineP/magiq.py
@@ -1,3 +1,9 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+
+cols = ["fLength","fWidth", "fSize", "fConc",  "fConcl", "fAsym", "fM3Long", "fM3Trans", "fAlpha", "fDist", "class"]
+
+df = pd.read_csv('./data/magic04.data', names=cols)
+
+df.head()
