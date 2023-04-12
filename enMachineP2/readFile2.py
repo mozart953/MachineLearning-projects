8e4655c8016defb04c24229e0ec51ff9a144f4d7
mozart953 <wilsonnianopop@gmail.com>
2023-04-11 20:11:46 -0600

    some changes KNN

--- enMachineP2/readFile2.py
+++ enMachineP2/readFile2.py
@@ -1,5 +1,8 @@
 from sklearn.neighbors import KNeighborsClassifier
 import readFile as rF
+from sklearn.metrics import classification_report
+
+
 
 knn_model = KNeighborsClassifier(n_neighbors=1)
 knn_model.fit(rF.x_train, rF.y_train)
@@ -8,4 +11,7 @@ KNeighborsClassifier(n_neighbors=1)
 
 y_pred= knn_model.predict(rF.x_test)
 
+
 y_pred
+
+print(classification_report(rF.y_test, y_pred))
