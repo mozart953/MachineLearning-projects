5c98dba43be525a1f6daf06a2a0d8d598d8850b4
mozart953 <wilsonnianopop@gmail.com>
2023-04-13 19:05:55 -0600

    changes on reunal network

--- enMachineP2/neuraNet.py
+++ enMachineP2/neuraNet.py
@@ -3,23 +3,32 @@ import readFile as rF
 
 import tensorflow as tf
 
-def plot_loss(history):
-    plt.plot(history.history['loss'], label = 'loss')
-    plt.plot(history.history['val_loss'], label = 'val_loss')
-    plt.xlabel('Epoch')
-    plt.ylabel('Binary crossentropy')
-    plt.legend()
-    plt.grid(True)
+def plot_history(history):
+    fig, (ax1, ax2) = plt.subplots(1,2, figsize=(10,8))
+    ax1.plot(history.history['loss'], label = 'loss')
+    ax1.plot(history.history['val_loss'], label = 'val_loss')
+    ax1.set_xlabel('Epoch')
+    ax1.set_ylabel('Binary crossentropy')
+    ax1.legend()
+    ax1.grid(True)
+
+    ax2.plot(history.history['accuracy'], label='accuracy')
+    ax2.plot(history.history['val_accuracy'], label='val_accuracy')
+    ax2.set_xlabel('Epoch')
+    ax2.set_ylabel('Accuracy')
+    ax2.grid(True)
+
     plt.show()
 
-def plot_accuracy(history):
-    plt.plot(history.history['accuracy'], label='accuracy')
-    plt.plot(history.history['val_accuracy'], label='val_accuracy')
-    plt.xlabel('Epoch')
-    plt.ylabel('Accuracy')
-    plt.legend()
-    plt.grid(True)
-    plt.show()
+# plot_history(history)
+# def plot_accuracy(history):
+#     plt.plot(history.history['accuracy'], label='accuracy')
+#     plt.plot(history.history['val_accuracy'], label='val_accuracy')
+#     plt.xlabel('Epoch')
+#     plt.ylabel('Accuracy')
+#     plt.legend()
+#     plt.grid(True)
+#     plt.show()
 
 def train_model(x_train, y_train, num_modes, dropout_prob, lr, batch_size, epochs):
 
@@ -39,7 +48,29 @@ def train_model(x_train, y_train, num_modes, dropout_prob, lr, batch_size, epoch
     )
     return nn_model, history
 
+epochs = 100
 
+
+least_val_loss = float('inf')
+least_loss_model = None
+for num_nodes in [16, 32, 64]:
+    for dropout_prob in [0, 0.2]:
+        for lr in [0.1, 0.005, 0.001]:
+            for batch_size in [32,64,128]:
+                print(f"{num_nodes} nodes, dropout {dropout_prob}, lr {lr}, batch_size {batch_size}" )
+                model, history = train_model(rF.x_train, rF.y_train, num_nodes, dropout_prob, lr, batch_size, epochs)
+                plot_history(history)
+                val_loss = model.evaluate(rF.x_valid, rF.y_valid)[0]
+                if val_loss < least_val_loss:
+                    least_val_loss = val_loss
+                    least_loss_model = model
+                # plot_accuracy(history)
+
+y_pred= least_loss_model.predict(rF.x_test)
+y_pred = (y_pred > 0.5).astype(int).reshape(-1,)
+y_pred
+
+print(classification_report(rF.y_test, y_pred))
 # history = nn_model.fit(
 #     rF.x_train, rF.y_train,
 #     epochs=100, batch_size=32, validation_split=0.2, verbose=0
