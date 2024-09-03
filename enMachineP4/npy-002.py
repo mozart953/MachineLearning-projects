f325852795cb6faba5cea778784a3170d433e882
mozart953 <wilsonnianopop@gmail.com>
2024-09-03 15:56:17 -0600

    frames

--- enMachineP4/npy-002.py
+++ enMachineP4/npy-002.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 numeros = [1,2,3,4,5,6,7,8,9,10,2,3]
 
@@ -37,3 +38,39 @@ def cuadrado(x):
 bb2=serie2.map(cuadrado)
 
 print(bb2)
+
+df2 = pd.DataFrame(
+        {
+            "A": 1.0,
+            "B": pd.Timestamp("20130102"),
+            "C": pd.Series(1, index=list(range(4)), dtype="float32"),
+            "D": np.array([3] * 4, dtype="int32"),
+            "E": pd.Categorical(["test", "train", "test", "train"]),
+            "F": "foo",
+        }
+    )
+print(df2)
+
+
+datos = {
+    "nombre":["Argentina", "Brazil", "Chile", "Colombia","Ecuador" ,"Paraguay", "Peru"],
+    "capital":["Buenos Aires", "Brasilia", "Santiago de Chile", "Bogota","Quito", "Asuncion", "Lima"],
+    "poblacion":[45380000, 212600000, 19120000,50880000, 17640000, 7133000, 32970000],
+    "sup. en k2":[2780000, 8516000, 756950, 1143000, 283560, 406752, 1285216]
+}
+
+paises = pd.DataFrame(datos)
+
+print(paises)
+
+print("Primeros 4")
+print(paises.head())
+print("Ultimos 3")
+print(paises.tail(3))
+
+paises2 = pd.DataFrame(datos, columns=["nombre", "poblacion"])
+
+print(paises2)
+
+paises3 = pd.DataFrame(datos, index=datos["nombre"], columns=["capital", "poblacion", "sup. en k2"])
+print(paises3)
